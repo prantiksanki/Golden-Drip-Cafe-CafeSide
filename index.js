3d815f5bb38a6c60b2cebd64cb25b5d4992eb61d
Prantik Sanki <prantiksanki@gmail.com>
2025-01-30 00:18:22 +0530

    Cancel button

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express() ; 
 const menu = require('./model/menu');
 const PORT = 80 ;
-
+const path = require("path");
 
 // const storage = multer.diskStorage({
 //     destination: (req, file, cb) => {
@@ -43,30 +43,48 @@ app.get("/add-menu" , (req, res) =>
     res.render("addMenu")  ;
 })  
 
-
   
 app.post("/add-menu", async (req, res) => {
-    const items = req.body;
-    // const file = req.file; 
-    // if (!file) {
-    //   return res.status(400).send("No file uploaded.");
-    // }
+  const items = req.body;
+  // const file = req.file; 
+  // if (!file) {
+  //   return res.status(400).send("No file uploaded.");
+  // }
+ 
+  console.log(items)
+
+  try {
+    const newMenuItem = await menu.create({
+      name: items.name,
+      description: items.description,
+      category: items.category,
+      price: parseFloat(items.price), 
+      quantity: parseFloat(items.quantity), 
+      image: items.image,
+    });
+
+    res.status(201).redirect("/")
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error saving menu item.");
+  }
+});
+
+
+  app.post("/removeMenu", async (req, res) => {
+    const { itemId } = req.body;
   
     try {
-      const newMenuItem = await menu.create({
-        name: items.name,
-        description: items.description,
-        category: items.category,
-        price: parseFloat(items.price), 
-        quantity: parseFloat(items.quantity), 
-        image: items.image,
-      });
-  
-      res.status(201).redirect("/")
+      await menu.findByIdAndDelete(itemId);
+      console.log(`Item ${itemId} deleted`);
+      res.status(200).json({ message: "Item successfully deleted" });
     } catch (error) {
-      console.error(error);
-      res.status(500).send("Error saving menu item.");
+      console.error("Error deleting item:", error);
+      res.status(500).json({ message: "Failed to delete item" });
     }
   });
+  
+
+
   
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
